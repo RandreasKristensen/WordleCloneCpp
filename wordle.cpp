1cdf8c5b3f959da3dae6082af8a62a0596559af5
RandreasKristensen <arandkristensen@gmail.com>
2025-03-24 21:26:55 +0100

    minor visual update

--- wordle.cpp
+++ wordle.cpp
@@ -80,6 +80,7 @@ int main() {
         while (guessesLeft > 0 && !win) {
             //Display the gamescreen
             system("CLS");
+            cout << endl << endl;
             displayGuesses(coloredGuesses);
             displayKeyboard(guessedLetters, yellowLetters, greenLetters, keyboard);
             //Display errorMessage if there is any, then reset
